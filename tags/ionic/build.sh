6120ea40c0586b655f7ad93e8e463287b9e92407
Tarsis de Lima <br.tmvdl@gmail.com>
2021-04-22 22:51:18 -0300

    Update tag Ionic

---
+++ tags/ionic/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+npm ci
+
+ionic build --prod --project=$NG_APP --release --wwwDir www
+
+cordova platform add android
+
+cordova build android --release --buildConfig
+
+cp /app/platforms/android/app/build/outputs/bundle/release/app-release.aab /builds
---
+++ tags/ionic/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ng serve $NG_APP -- --host=0.0.0.0 --port=8100
