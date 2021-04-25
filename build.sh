427e2151f869cf640b1e4367a6a3fdbb1212d3bc
Tarsis de Lima <br.tmvdl@gmail.com>
2021-04-25 09:46:55 -0300

    Update build workflow

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 CURRENT_DIR=$(pwd)
+YMD=$(date +%Y%m%d)
+YM=$(date +%Y%m)
 
 for TAG in $@; do
   docker build -t tmvdl/android:$TAG $CURRENT_DIR/tags/$TAG/
+  docker tag tmvdl/android:$TAG tmvdl/android:$TAG-$YMD
+  docker tag tmvdl/android:$TAG tmvdl/android:$TAG-$YM
 done
