907dcc89d518285e03fef02a299ddc7fd80bd179
Tarsis de Lima <br.tmvdl@gmail.com>
2021-04-25 11:32:32 -0300

    Adding creation of file keystore on tag Cordova

--- tags/cordova/build.sh
+++ tags/cordova/build.sh
@@ -4,6 +4,8 @@ npm ci
 
 cordova platform add android
 
-cordova build android --release --buildConfig
+[[ $MAKEKEYFILE ]] && yes | keytool -genkey -v -alias $KEYALIAS -keystore $KEYFILE -storepass $STOREPASS -keypass $KEYPASS -validity 7 -keysize 2048 -keyalg RSA
+
+cordova build android --release -- --alias=$KEYALIAS --keystore=$KEYFILE --storePassword=$STOREPASS --password=$KEYPASS
 
 cp /app/platforms/android/app/build/outputs/bundle/release/app-release.aab /builds
