f5bd95089b36dc2fcbcff8c19cf9bfa4a93fa3af
Tarsis de Lima <br.tmvdl@gmail.com>
2021-04-26 21:58:10 -0300

    Fixing build of Ionic

--- tags/ionic/build.sh
+++ tags/ionic/build.sh
@@ -8,4 +8,4 @@ cordova platform add android
 
 cordova build android --release --buildConfig
 
-cp /app/platforms/android/app/build/outputs/bundle/release/app-release.aab /builds
+cp /app/platforms/android/app/build/outputs/ /builds -r
