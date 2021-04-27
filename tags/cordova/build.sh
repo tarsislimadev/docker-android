3330f7bcf582dcaf161529a53980bd7456efae3b
Tarsis de Lima <br.tmvdl@gmail.com>
2021-04-26 21:06:35 -0300

    Removing "make key file" condition

--- tags/cordova/build.sh
+++ tags/cordova/build.sh
@@ -4,7 +4,7 @@ npm ci
 
 cordova platform add android
 
-[[ $MAKEKEYFILE ]] && yes | keytool -genkey -v -alias $KEYALIAS -keystore $KEYFILE -storepass $STOREPASS -keypass $KEYPASS -validity 7 -keysize 2048 -keyalg RSA
+yes | keytool -genkey -v -alias $KEYALIAS -keystore $KEYFILE -storepass $STOREPASS -keypass $KEYPASS -validity 7 -keysize 2048 -keyalg RSA
 
 cordova build android --release -- --alias=$KEYALIAS --keystore=$KEYFILE --storePassword=$STOREPASS --password=$KEYPASS
 
