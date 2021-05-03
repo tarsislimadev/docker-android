25ff1979226b4f417c20c394b05ee4d6ca33b4a5
Tarsis de Lima <br.tmvdl@gmail.com>
2021-05-02 21:15:57 -0300

    Adding .apk compilation at tag Ionic

--- tags/ionic/build.sh
+++ tags/ionic/build.sh
@@ -6,6 +6,20 @@ ionic build --prod --project=$NG_APP --release --wwwDir www
 
 cordova platform add android
 
-cordova build android --release --buildConfig
+yes | keytool -genkey -v -alias $CORDOVA_KEYALIAS -keystore $CORDOVA_KEYFILE -storepass $CORDOVA_STOREPASS -keypass $CORDOVA_KEYPASS -validity $CORDOVA_VALIDITY -keysize 2048 -keyalg RSA
+
+cordova build android --release -- --alias=$CORDOVA_KEYALIAS --keystore=$CORDOVA_KEYFILE --storePassword=$CORDOVA_STOREPASS --password=$CORDOVA_KEYPASS
 
 cp /app/platforms/android/app/build/outputs/ /builds -r
+
+# AAB --> APK
+
+AABS=$(find /builds -name *.aab)
+
+for AAB in $AABS; do
+
+APK=$(echo $AAB | sed -s s/.aab/.apks/)
+
+java -jar /env/bundletool-all-1.6.0.jar build-apks --bundle=$AAB --output=$APK --ks=$CORDOVA_KEYFILE --ks-pass=pass:$CORDOVA_KEYPASS --ks-key-alias=$CORDOVA_KEYALIAS
+
+done
