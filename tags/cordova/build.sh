726590ab150c9f1b6c9baa0c23fe7af2cf79fb50
Tarsis de Lima <br.tmvdl@gmail.com>
2021-10-06 15:56:40 -0300

    Updating Android SDK

--- tags/cordova/build.sh
+++ tags/cordova/build.sh
@@ -10,7 +10,7 @@ cordova platform add android
 
 yes | keytool -genkey -v -alias $CORDOVA_KEYALIAS -keystore $CORDOVA_KEYFILE -storepass $CORDOVA_STOREPASS -keypass $CORDOVA_KEYPASS -validity $CORDOVA_VALIDITY -keysize 2048 -keyalg RSA
 
-cordova build android --release -- --alias=$CORDOVA_KEYALIAS --keystore=$CORDOVA_KEYFILE --storePassword=$CORDOVA_STOREPASS --password=$CORDOVA_KEYPASS
+cordova build android --release -- --alias=$CORDOVA_KEYALIAS --keystore=$CORDOVA_KEYFILE --storePassword=$CORDOVA_STOREPASS --password=$CORDOVA_KEYPASS --gradleArg=-PcdvCompileSdkVersion=30
 
 cp /app/platforms/android/app/build/outputs/ /builds -r
 
